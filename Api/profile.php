0daf34e115c847a351d6a9c1debf4beef7208548
frankz168 <franky.sutanto93@gmail.com>
2020-10-05 21:21:49 +0700

    add update profile function api.

--- Api/profile.php
+++ Api/profile.php
@@ -1,1 +1,75 @@
 <?php
+header("Content-type: application/json");
+//include_once "Database/koneksi.php";
+include_once '../Database/database.php';
+include_once '../Controller/users.php';
+
+$data = json_decode(file_get_contents('php://input'), true);
+$email=$data['email'];
+$idcardnumber=$data['idcardnumber'];
+$placeofbirth=$data['placeofbirth'];
+$dateofbirth=$data['dateofbirth'];
+$gender=$data['gender'];
+$religion=$data['religion'];
+$ImageKTP=$data['ImageKTP'];
+$ImageSelfie=$data['ImageSelfie'];
+$StatusMarriage=$data['StatusMarriage'];
+$Education=$data['Education'];
+$TaxID=$data['TaxID'];
+$ImageFamilyMemberCard=$data['ImageFamilyMemberCard'];
+$Occupation=$data['Occupation'];
+$Fields=$data['Fields'];
+$Position=$data['Position'];
+$StatusOfEmployment=$data['StatusOfEmployment'];
+$ProofOfEmployment=$data['ProofOfEmployment'];
+$ProofOfIncome=$data['ProofOfIncome'];
+$ProofOfBusiness=$data['ProofOfBusiness'];
+$ProofOfBusinessIncome=$data['ProofOfBusinessIncome'];
+$Type=$data['Type'];
+$Status=$data['Status'];
+$Village_ID=$data['Village_ID'];
+$Province_ID=$data['Province_ID'];
+$Street=$data['Street'];
+$Number=$data['Number'];
+$RT=$data['RT'];
+$RW=$data['RW'];
+$City=$data['City'];
+$SubDistrict=$data['SubDistrict'];
+$PostalCode=$data['PostalCode'];
+$ResidentialStatus=$data['ResidentialStatus'];
+$Duration=$data['Duration'];
+$ProofOfResidence=$data['ProofOfResidence'];
+$Name=$data['Name'];
+$Phone=$data['Phone'];
+$Relationship=$data['Relationship'];
+
+// get database connection
+$database = new Database();
+$db = $database->getConnection();
+// prepare user object
+$user = new Users($db);
+
+$stmt = $user->UpdateProfile($email, $idcardnumber, $placeofbirth, $dateofbirth, $gender, $religion, $ImageKTP, $ImageSelfie,
+    $StatusMarriage, $Education, $TaxID, $ImageFamilyMemberCard,
+    $Occupation, $Fields, $Position, $StatusOfEmployment,
+    $ProofOfEmployment, $ProofOfIncome, $ProofOfBusiness,
+    $ProofOfBusinessIncome, $Type, $Status, $Village_ID, $Province_ID, $Street, $Number, $RT, $RW, $City, $SubDistrict, $PostalCode,
+    $ResidentialStatus, $Duration, $ProofOfResidence,
+    $Name, $Phone, $Relationship);
+if($stmt->rowCount() > 0){
+    // create array
+    $profile_arr=array(
+        "success" => 1,
+        "message" => "Successfully Update Profile!"
+    );
+}
+else{
+    $profile_arr=array(
+        "success" => 0,
+        "message" => "Update Profile Failed!",
+    );
+}
+// make it json format
+print_r(json_encode($respass_arr));
+
+?>
--- Controller/users.php
+++ Controller/users.php
@@ -96,4 +96,73 @@ class Users
             print_r($e->getMessage());
         }
     }
+
+    public function UpdateProfile($email, $idcardnumber, $placeofbirth, $dateofbirth, $gender, $religion, $ImageKTP, $ImageSelfie,
+                                  $StatusMarriage, $Education, $TaxID, $ImageFamilyMemberCard,
+                                  $Occupation, $Fields, $Position, $StatusOfEmployment,
+                                  $ProofOfEmployment, $ProofOfIncome, $ProofOfBusiness,
+                                  $ProofOfBusinessIncome, $Type, $Status, $Village_ID, $Province_ID, $Street, $Number, $RT, $RW, $City, $SubDistrict, $PostalCode,
+                                  $ResidentialStatus, $Duration, $ProofOfResidence,
+                                  $Name, $Phone, $Relationship){
+
+        // call sp for update account profile
+        try {
+            require_once '../Database/database.php';
+            $query = ("CALL UpdateProfileAccount('$email', '$idcardnumber', '$placeofbirth', '$dateofbirth', '$gender', '$religion', '$ImageKTP', '$ImageSelfie',
+                                  '$StatusMarriage', '$Education', '$TaxID', '$ImageFamilyMemberCard',
+                                  '$Occupation', '$Fields', '$Position', '$StatusOfEmployment',
+                                  '$ProofOfEmployment', '$ProofOfIncome', '$ProofOfBusiness',
+                                  '$ProofOfBusinessIncome', '$Type', '$Status', '$Village_ID', '$Province_ID', '$Street', '$Number', '$RT', '$RW', '$City', '$SubDistrict', '$PostalCode',
+                                  '$ResidentialStatus', '$Duration', '$ProofOfResidence',
+                                  '$Name', '$Phone', '$Relationship')") ;
+            // prepare query statement
+            $stmt = $this->conn->prepare($query);
+            $stmt->execute();
+            $stmt->bindParam('email',$email);
+            $stmt->bindParam('idcardnumber',$idcardnumber);
+            $stmt->bindParam('placeofbirth',$placeofbirth);
+            $stmt->bindParam('dateofbirth',$dateofbirth);
+            $stmt->bindParam('gender',$gender);
+            $stmt->bindParam('religion',$religion);
+            $stmt->bindParam('ImageKTP',$ImageKTP);
+            $stmt->bindParam('ImageSelfie',$ImageSelfie);
+            $stmt->bindParam('StatusMarriage',$StatusMarriage);
+            $stmt->bindParam('Education',$Education);
+            $stmt->bindParam('TaxID',$TaxID);
+            $stmt->bindParam('ImageFamilyMemberCard',$ImageFamilyMemberCard);
+            $stmt->bindParam('Occupation',$Occupation);
+            $stmt->bindParam('Fields',$Fields);
+            $stmt->bindParam('Position',$Position);
+            $stmt->bindParam('StatusOfEmployment',$StatusOfEmployment);
+            $stmt->bindParam('ProofOfEmployment',$ProofOfEmployment);
+            $stmt->bindParam('ProofOfIncome',$ProofOfIncome);
+            $stmt->bindParam('ProofOfBusiness',$ProofOfBusiness);
+            $stmt->bindParam('ProofOfBusinessIncome',$ProofOfBusinessIncome);
+            $stmt->bindParam('Type',$Type);
+            $stmt->bindParam('Status',$Status);
+            $stmt->bindParam('Village_ID',$Village_ID);
+            $stmt->bindParam('Province_ID',$Province_ID);
+            $stmt->bindParam('Street',$Street);
+            $stmt->bindParam('Number',$Number);
+            $stmt->bindParam('RT',$RT);
+            $stmt->bindParam('RW',$RW);
+            $stmt->bindParam('City',$City);
+            $stmt->bindParam('SubDistrict',$SubDistrict);
+            $stmt->bindParam('PostalCode',$PostalCode);
+            $stmt->bindParam('ResidentialStatus',$ResidentialStatus);
+            $stmt->bindParam('Duration',$Duration);
+            $stmt->bindParam('ProofOfResidence',$ProofOfResidence);
+            $stmt->bindParam('Name',$Name);
+            $stmt->bindParam('Phone',$Phone);
+            $stmt->bindParam('Relationship',$Relationship);
+
+            return $stmt;
+
+        }
+        catch(Exception $e)
+        {
+            print_r($e->getMessage());
+        }
+
+    }
 }
