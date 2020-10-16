71464a1cde3f50205442989814106620869f75cb
vita1288 <jovitasutanto97@gmail.com>
2020-10-16 17:17:57 +0700

    example uploading image and fixing insert loan

--- Api/insertloan.php
+++ Api/insertloan.php
@@ -4,34 +4,44 @@ header("Content-type: application/json");
 include_once '../Database/database.php';
 include_once '../Controller/loan.php';
 
+
 $data = json_decode(file_get_contents('php://input'), true);
-//$email=$data['email'];
-//$periodtime=$data['periodtime'];
-//$interest=$data['interest'];
-//$amount_without_interest=$data['amount_without_interest'];
-//$totalamount=$data['totalamount'];
-//$Reason=$data['Reason'];
+$email=$_GET['email'];
+$product_id = $data['product_id'];
+$loan_status_id = $data['loan_status_id'];
+$AgreementDate = $data['AgreementDate'];
+$DisbursementDate = $data['DisbursementDate'];
+$periodtime =$data['periodtime'];
+$interest =$data['interest'];
+$amount_without_interest=$data['amount_without_interest'];
+$totalamount =$data['totalamount'];
+$DueAmount = $data['DueAmount'];
+$DateAmount = $data['DateAmount'];
+$DueDateAmount = $data['DueDateAmount'];
+$LoanQuality = $data['LoanQuality'];
+$LoanStatus = $data['LoanStatus'];
+$Reason=$data['Reason'];
 
 
 //CALL InsertLoan('franky.sutanto93@gmail.com','1', '1', '2020-10-15', '2020-10-15', '6', '1.5', '4000000', '4500000',
 //    '4500000', '2020-10-15', '2021-04-10', 'Good', 'Submit',
 //    'test borrow')
 
-$email = 'jovitasutanto98@gmail.com';
-$product_id = '1';
-$loan_status_id = '1';
-$AgreementDate = '2020-10-10';
-$DisbursementDate = '2020-10-10';
-$periodtime = '12';
-$interest = '1.5';
-$amount_without_interest = 20000000;
-$totalamount = 26000000;
-$DueAmount = 20000000;
-$DateAmount = '2020-10-10';
-$DueDateAmount  = '2020-10-10';
-$LoanQuality = 'Good';
-$LoanStatus = 'InProgress';
-$Reason = "ingin bayar uang pangkal";
+//$email = 'jovitasutanto98@gmail.com';
+//$product_id = '1';
+//$loan_status_id = '1';
+//$AgreementDate = '2020-10-10';
+//$DisbursementDate = '2020-10-10';
+//$periodtime = '12';
+//$interest = '1.5';
+//$amount_without_interest = 20000000;
+//$totalamount = 26000000;
+//$DueAmount = 20000000;
+//$DateAmount = '2020-10-10';
+//$DueDateAmount  = '2020-10-10';
+//$LoanQuality = 'Good';
+//$LoanStatus = 'InProgress';
+//$Reason = "ingin bayar uang pangkal";
 
 
 
