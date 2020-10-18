07051b34a6025a35f8b4af31d3fd705316d6f1ba
frankz168 <franky.sutanto93@gmail.com>
2020-10-18 20:30:36 +0700

    enchance insertloan api.

--- Api/insertloan.php
+++ Api/insertloan.php
@@ -6,40 +6,42 @@ include_once '../Controller/loan.php';
 
 
 $data = json_decode(file_get_contents('php://input'), true);
-$email=$_GET['email'];
-$product_id = $data['product_id'];
-$loan_status_id = $data['loan_status_id'];
-$AgreementDate = $data['AgreementDate'];
-$DisbursementDate = $data['DisbursementDate'];
-$periodtime =$data['periodtime'];
-$interest =$data['interest'];
-$amount_without_interest=$data['amount_without_interest'];
-$DueAmount = $data['DueAmount'];
-$LoanQuality = $data['LoanQuality'];
-$LoanStatus = $data['LoanStatus'];
-$Reason=$data['Reason'];
+//$email=$_GET['email'];
+//$product_id = $data['product_id'];
+//$loan_status_id = $data['loan_status_id'];
+//$AgreementDate = $data['AgreementDate'];
+//$DisbursementDate = $data['DisbursementDate'];
+//$periodtime =$data['periodtime'];
+//$interest =$data['interest'];
+//$amount_without_interest=$data['amount_without_interest'];
+//$DueAmount = $data['DueAmount'];
+//$LoanQuality = $data['LoanQuality'];
+//$LoanStatus = $data['LoanStatus'];
+//$Reason=$data['Reason'];
 
 
 //CALL InsertLoan('franky.sutanto93@gmail.com','1', '1', '2020-10-15', '2020-10-15', '6', '1.5', '4000000', '4500000',
 //    '4500000', '2020-10-15', '2021-04-10', 'Good', 'Submit',
 //    'test borrow')
 
-//$email = 'jovitasutanto98@gmail.com';
-//$product_id = '1';
-//$loan_status_id = '1';
-//$AgreementDate = '2020-10-10';
-//$DisbursementDate = '2020-10-10';
-//$periodtime = 12;
-//$interest = '1.5';
-//$amount_without_interest = 5200000;
-//$DueAmount = 5200000;
-//$LoanQuality = 'Good';
-//$LoanStatus = 'InProgress';
-//$Reason = "ingin bayar uang pangkal";
+$email = 'jovitasutanto98@gmail.com';
+$product_id = '1';
+$loan_status_id = '1';
+$AgreementDate = '2020-10-10';
+$DisbursementDate = '2020-10-10';
+$periodtime = 12;
+$interest = '1.5';
+$amount_without_interest = 5200000;
+$DueAmount = 5200000;
+$LoanQuality = 'Good';
+$LoanStatus = 'In Process';
+$Reason = "ingin bayar uang pangkal";
 
 //can't be deleted.
 $InvoiceStatus = "Not Paid";
 
+$successbool = 0;
+$message ="Submit Loan Failed!. Your loan data form still outstanding";
 
 // get database connection
 $database = new Database();
@@ -84,27 +86,29 @@ if($stmt->rowCount() > 0){
             //$DueDateAmount = $row['DueDateAmount'];
             $DueDateAmountInvoice  = new DateTime();
             for ($x = -1; $x <= $periodtime; $x++) {
-                echo "No: $x \n";
+                //echo "No: $x \n";
                 if($x > 0){
                     $DueDateAmountInvoice->modify("+1 month"); // or you can use '-90 day' for deduct
                 }
                 $stmt2 = $loan->InsertInvoice($loanid, $accid, $x, $totalamountpermonth, $InvoiceStatus, $DueDateAmountInvoice ->format('Y-m-d'));
                 if($stmt2->rowCount() > 0){
-                    echo "Success: $x \n";
+                    $successbool = 1;
+                    $message = "Successfully Submit Loan! Loan will be process by admin.";
+                    //echo "Success: $x \n";
                 }
             }
-        }
+    }
 
     // create array
     $loan_arr=array(
-        "success" => 1,
-        "message" => "Successfully Submit Loan! Loan will be process by admin."
+        "success" => $successbool,
+        "message" => $message
     );
 }
 else{
     $loan_arr=array(
-        "success" => 0,
-        "message" => "Submit Loan Failed!",
+        "success" => $successbool,
+        "message" => $message
     );
 }
 // make it json format
