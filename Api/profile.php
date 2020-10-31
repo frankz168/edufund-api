fb8fdaab6c5e20181ee2eb3570bfb76ab51bbf3b
frankz168 <franky.sutanto93@gmail.com>
2020-10-31 20:50:22 +0700

    example insert db file target profile.

--- Api/profile.php
+++ Api/profile.php
@@ -80,6 +80,23 @@ $Relationship=$data['Relationship'];
 //$Phone='0812123';
 //$Relationship='No';
 
+if($ImageKTP != null){
+    $target_dir = 'uploads/';
+    $target_file = $target_dir . basename($_FILES['fileToUpload']['name']);
+
+    $status = array();
+    if (move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $target_file)) {
+        $status['kode']=1;
+        $status['deskripsi']='upload success';
+        $ImageKTP = $target_file ;
+    } else {
+        $status['kode']=0;
+        $status['deskripsi']='upload failed';
+        $ImageKTP = null;
+    }
+}
+
+
 // get database connection
 $database = new Database();
 $db = $database->getConnection();
